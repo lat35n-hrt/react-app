f378e708b271776b3534d4fe281a37c493dcd3bb
Michael White <hrt666.cs@gmail.com>
2023-06-20 12:21:39 +0900

    feat:add custom.css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
-import 'custom.css'
+import './custom.css';
 
 class App extends React.Component {
 
@@ -43,21 +43,16 @@ class App extends React.Component {
 
     return (
       <div className="container">
-        {/* <header className="mt-3 mb-4"> 
-          <h1>Data Generated from Django</h1> 
-        </header> */}
-
         <header style={{ backgroundColor: '#F36824', color: '#FFFFFF', padding: '20px', marginBottom: '20px', fontFamily: 'jazzy-font' }}>
           Data Generated from Django
         </header>
-
         <hr></hr>
         {error ? (
           <div style={{ backgroundColor: '#343A40', color: '#FFFFFF', padding: '10px', marginBottom: '20px' }}>
             {error}
           </div> // Display the error message
         ) : (
-          <table className="table">
+          <table className="table custom-table">
             <thead>
               <tr>
                 <th style={{ color: '#F36824' }}>Employee</th>
@@ -66,7 +61,7 @@ class App extends React.Component {
             </thead>
             <tbody>
               {details.map((output, id) => (
-                <tr key={id} style={{ backgroundColor: id % 2 === 0 ? '#FFFFFF' : '#F8F9FA' }}>
+                <tr key={id} className={id % 2 === 0 ? 'custom-row-even' : 'custom-row-odd'}>
                   <td>{output.employee}</td>
                   <td>{output.department}</td>
                 </tr>
